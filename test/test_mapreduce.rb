17f7df61b14c8ecb288e0237712f2609de14e4cd
David Chen <mvjome@gmail.com>
2013-08-30 23:50:35 +0800

    Add test_multiple_dimensions_output_without_time_column

--- test/test_mapreduce.rb
+++ test/test_mapreduce.rb
@@ -6,7 +6,14 @@ class TestMapReduce < Test::Unit::TestCase
   def setup
   end
 
-  def test_hotest_items_mapreduce
+  def test_multiple_dimensions_output_without_time_column
+    cron = Statlysis.always['mongoid']['code'][0]
+    assert_equal cron.time_column, false
+    assert_equal cron.time_unit, false
+    assert_equal cron.stat_table_name, 'timely_codegistmongoids_a'
+
+    cron.run
+    assert_equal cron.output.detect {|h| h[:author] == 'mvj3' }[:c].to_i, cron.multiple_dataset.sources.first.where(:author => 'mvj3').count
   end
 
 
