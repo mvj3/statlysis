74315c27d6e250e545767d6841caee8c88c600a3
David Chen <mvjome@gmail.com>
2015-04-19 05:02:48 +0800

    Fix mongoid#timestamps api warning

--- test/migrate/1_active_record.rb
+++ test/migrate/1_active_record.rb
@@ -2,7 +2,7 @@ class CreateActiveRecord < ActiveRecord::Migration
   create_table :code_gists do |t|
     t.string  :description
     t.integer :user_id
-    t.timestamps
+    t.timestamps null: true
     t.string :author
     t.integer :fav_count
   end
